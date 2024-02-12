a436532bf5068b1e3831fb1724ba70d53d2e043b
IdaNagel <143513463+IdaNagel@users.noreply.github.com>
2024-02-12 14:45:38 +0100

    Hele koden for kjøp billet er ferdig

--- src/main/resources/static/oblig1.js
+++ src/main/resources/static/oblig1.js
@@ -1,6 +1,6 @@
-let billeter = [];
+let billetter = [];
 
-function kjopBillett(){
+function kjopBillett() {
     const film = document.getElementById("film").value;
     const santall = document.getElementById("antall").value;
     const fornavn = document.getElementById("fornavn").value;
@@ -9,34 +9,58 @@ function kjopBillett(){
     const epost = document.getElementById("epost").value;
 
     const antall = Number(santall);
-    const telefonnr = Number (stelefonnr);
+    const telefonnr = Number(stelefonnr);
 
-    if (isNaN(antall) || antall<1){
+    if (isNaN(antall) || antall < 1) {
         document.getElementById("feilantall").innerHTML = "Må skrive noe inn i antall";
     }
-    if (isNaN(telefonnr) || telefonnr<1){
+    if (isNaN(telefonnr) || telefonnr < 1) {
         document.getElementById("feiltelefonnr").innerHTML = "Må skrive inn tall i telefonnummer"
     }
-    if (fornavn.length==0){
+    if (fornavn.length == 0) {
         document.getElementById("feilfornavn").innerHTML = "Må skrive noe inn i fornavn";
     }
-    if (etternavn.length==0){
+    if (etternavn.length == 0) {
         document.getElementById("feiletternavn").innerHTML = "Må skrive noe inn i etternavn";
     }
-    if (epost.length==0){
+    if (epost.length == 0) {
         document.getElementById("feilepost").innerHTML = "Må skrive noe inn i epost";
     }
+    else {
+        const billett = {
+            film: film,
+            antall: antall,
+            fornavn: fornavn,
+            etternavn: etternavn,
+            telefonnr: telefonnr,
+            epost: epost
 
-    const billett = {
-        film : film,
-        antall : antall,
-        fornavn : fornavn,
-        etternavn : etternavn,
-        telefonnr : telefonnr,
-        epost : epost
+        }
 
+        billetter.push(billett);
     }
 
 
+    let ut = "<table><tr>" +
+        "<th>Film</th><th>Antall</th><th>Fornavn</th><th>Etternavn</th><th>Telefonnummer</th><th>Epost</th>" +
+        "</tr>";
+    for (let b of billetter) {
+        ut += "<tr>";
+        ut += "<td>" + b.film + "</td><td>" + b.antall + "</td><td>" + b.fornavn + "</td><td>" + b.etternavn + "</td><td>" + b.telefonnr +
+            "</td><td>" + b.epost + "</td>";
+        ut += "</tr>";
+    }
+
+    document.getElementById("allebilletter").innerHTML = ut;
+
+    document.getElementById("antall").value = "";
+    document.getElementById("fornavn").value = "";
+    document.getElementById("etternavn").value = "";
+    document.getElementById("telefonnr").value = "";
+    document.getElementById("epost").value = "";
+
+}
+
+function slettBilletter(){
 
 }
